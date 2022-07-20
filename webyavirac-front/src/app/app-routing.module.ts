a5b3ad8c1fbe10ad1902e5412d00d470175ea546
Nicole-Titumaita <71206667+Nicole-Titumaita@users.noreply.github.com>
2022-07-20 15:01:32 -0500

    dashbord

--- webyavirac-front/src/app/app-routing.module.ts
+++ webyavirac-front/src/app/app-routing.module.ts
@@ -1,25 +1,15 @@
-// import { NgModule } from '@angular/core';
-// import { RouterModule, Routes } from '@angular/router';
-// // import { DashboardComponent } from './dashboard/dashboard.component';
-
-// const routes: Routes = [
-// 	// { path:'', redirectTo:'/home', pathMatch:'full'},
-//   // {path:'home' , component: DashboardComponent},
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
-
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path:'', redirectTo:'/home', pathMatch:'full'},
+  {path:'home' , component: DashboardComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- webyavirac-front/src/app/app.component.ts
+++ webyavirac-front/src/app/app.component.ts
@@ -1,19 +1,5 @@
-// import { Component } from '@angular/core';
-// // import {DomSanitizer} from '@angular/platform-browser';
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
-//   title = 'webyavirac-front';
-// 	// cssUrl: string;
-// 	// constructor(public sanitizer: DomSanitizer){
-// 	// 	this.cssUrl = '/assets/styles1.css';
-// 	// }
-// }
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -22,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webyavirac-front';
+	cssUrl: string;
+	constructor(public sanitizer: DomSanitizer){
+		this.cssUrl = '/assets/styles1.css';
+	}
 }
