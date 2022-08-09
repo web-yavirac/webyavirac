2a2176e69cc43dd2b13d4b27eeeff00a371d4554
Nicole-Titumaita <71206667+Nicole-Titumaita@users.noreply.github.com>
2022-08-09 15:10:05 -0500

    prueba 1

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	plugins: [require("daisyui")],
+	content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
