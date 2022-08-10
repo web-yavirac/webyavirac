e7a1b0af123a59a33f19e7324d68b8147bc608f1
Nicole-Titumaita <71206667+Nicole-Titumaita@users.noreply.github.com>
2022-08-10 15:53:18 -0500

    arreglos

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Noticias } from '../models/noticias';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NewsService {
+export class ProductoService {
+  url = 'http://localhost:4000/api/noticias/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProductos(): Observable<any> {
+    return this.http.get(this.url);
+  }
+
+  eliminarNoticia(id: string): Observable<any> {
+    return this.http.delete(this.url + id);
+  }
+
+  guardarNoticia(producto: Noticias): Observable<any> {
+    return this.http.post(this.url, producto);
+  }
+
+  obtenerProducto(id: string): Observable<any> {
+    return this.http.get(this.url + id);
+  }
 }
